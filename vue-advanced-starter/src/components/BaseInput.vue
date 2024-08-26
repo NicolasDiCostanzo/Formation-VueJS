16ea015ee16bb66c8007ed7d664f220b470c211b
Nicolas Di Costanzo <ndicostanzo@wanadev.fr>
2024-08-26 16:08:16 +0200

    PlanForm inputs update temp value

--- vue-advanced-starter/src/components/BaseInput.vue
+++ vue-advanced-starter/src/components/BaseInput.vue
@@ -1,19 +1,43 @@
 <script lang="ts" setup>
-// TODO
+defineOptions({
+  inheritAttrs: false,
+})
+defineProps<{
+  label: string
+  textarea?: boolean
+  required: boolean
+  maxLength?: number
+}>()
+
+const modelValue = defineModel<string>({ required: true })
 
 const randomId = `input-${Math.round(Math.random() * 10000)}`
 </script>
 
 <template>
-  <div class="flex flex-col gap-1">
+  <div
+    class="flex flex-col gap-1" v-bind="{
+      style: $attrs.style,
+      class: $attrs.class,
+    }"
+  >
     <label :for="randomId">
       {{ label }}
+      <span v-if="required" class="text-red-500">*</span>
     </label>
-    <input
-      v-bind="$attrs"
+    <component
+      :is="textarea ? 'textarea' : 'input'"
+      v-bind="{
+        ...$attrs,
+        style: undefined,
+        class: undefined,
+      }"
       :id="randomId"
-      ref="inputEl"
+      :required
+      :value="modelValue"
       class="border border-gray-300 rounded p-2 focus:border-primary-500 outline-none"
+      :max-length
+      @input="modelValue = $event.target.value"
     />
   </div>
 </template>
--- vue-advanced-starter/src/components/PlanForm.vue
+++ vue-advanced-starter/src/components/PlanForm.vue
@@ -1,15 +1,41 @@
 <script lang="ts" setup>
-// TODO
+import { reactive } from 'vue'
+
+defineProps<{
+  error?: Error | string | null
+}>()
+
+const temp = reactive({
+  name: '',
+  description: '',
+})
 </script>
 
 <template>
   <form class="flex flex-col gap-6">
-    <!-- Inputs -->
+    <pre> {{ temp }}</pre>
+    <BaseInput
+      v-model="temp.name"
+      required
+      label="Name"
+      maxlength="50"
+      placeholder="My plan's name"
+    />
+
+    <BaseInput
+      v-model="temp.description"
+      required
+      label="Description"
+      textarea
+      maxlength="200"
+    />
 
     <div class="flex items-center gap-6 [&>*]:flex-1">
-      <!-- Buttons -->
+      <BaseButton type="submit">
+        Submit
+      </BaseButton>
     </div>
 
-    <!-- <BaseError :error /> -->
+    <BaseError :error />
   </form>
 </template>
