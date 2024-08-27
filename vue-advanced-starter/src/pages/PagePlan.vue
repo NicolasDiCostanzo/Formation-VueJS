5e534976bb23a841008c46bbe089232c9f1c1b90
Nicolas Di Costanzo <ndicostanzo@wanadev.fr>
2024-08-27 09:31:28 +0200

    update registered plans

--- vue-advanced-starter/src/pages/PagePlan.vue
+++ vue-advanced-starter/src/pages/PagePlan.vue
@@ -1,5 +1,35 @@
 <script lang="ts" setup>
-// TODO
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import type { Plan } from '@/util/types'
+
+defineProps<{
+  error: string | null
+}>()
+
+const router = useRouter()
+
+const route = useRoute()
+const plan = ref<Plan>()
+plan.value = await fetch(`/api/plans/${route.params.id}`).then(r => r.json())
+
+async function updatePlan() {
+  const result = await fetch(`/api/plans/${plan.value!.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(plan.value),
+  }).then(r => r.json())
+  plan.value = result
+
+  router.push({
+    name: 'plans',
+    params: {
+      id: plan.value?.id,
+    },
+  })
+}
 </script>
 
 <template>
@@ -8,6 +38,15 @@
       Edit Plan
     </h1>
 
-    <!-- TODO Form here -->
+    <PlanForm
+      v-if="plan"
+      v-model:name="plan.name"
+      v-model:description="plan.description"
+      @submit="updatePlan()"
+    />
+    <BaseError
+      v-else
+      :error
+    />
   </div>
 </template>
